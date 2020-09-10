76febd8f63c460ed10b1c60430f5d4ebc0c0f294
χєησѕ <67038345+xen0s-0x00@users.noreply.github.com>
2020-09-11 03:52:16 +0530

    Update msfinstall.sh

--- msfinstall.sh
+++ msfinstall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 apt -y update && apt -y upgrade
-apt install curl
 curl -LO https://github.com/termux/termux-packages/files/3995119/metasploit_5.0.65-1_all.deb.gz
 gunzip metasploit_5.0.65-1_all.deb.gz
 dpkg -i metasploit_5.0.65-1_all.deb
