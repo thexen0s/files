fd375ed69422f88bbd67cf2555972e2ddd029188
χєησѕ <67038345+xen0s-0x00@users.noreply.github.com>
2020-09-11 03:58:03 +0530

    Update msfinstall.sh

--- msfinstall.sh
+++ msfinstall.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+pkg install -y ruby
+gem install bundler:1.17.3
 curl -LO https://github.com/termux/termux-packages/files/3995119/metasploit_5.0.65-1_all.deb.gz
 gunzip metasploit_5.0.65-1_all.deb.gz
 dpkg -i metasploit_5.0.65-1_all.deb
 apt -f install
 apt -y update
+curl -LO https://raw.githubusercontent.com/th3unkn0n/extra/master/msfdb
+chmod +x msfdb
+./msfdb start
